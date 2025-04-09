2ffa89339a6878a56949247d6cb49d1593f82991
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-04-09 15:09:29 +0200

    draft user connection

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,12 +1,29 @@
 const {AuthModel} = require("../models/authModel");
+const bcrypt = require("bcrypt");
+
 
 const AuthController = {
   signUp: async (req, res) => {
-    const user = await AuthModel.createUser();
+    const hashedPassword = await bcrypt.hash("1234", 10);
+    const user = await AuthModel.createUser(hashedPassword);
     console.log(user);
   },
+
+  logIn: async (req, res) => {
+    const email = "laura.tr@gmail.com"
+    const password = "1234"
+    const userInfo = await AuthModel.logIn(email);
+
+    const passwordMatch = await bcrypt.compare(password, userInfo.password);
+        if (!passwordMatch) {
+            return "Identifiants incorrects"
+        } else {
+            console.log("Identification réussie!")
+        }
+  }
 }
 
-AuthController.signUp();
+//AuthController.signUp();
+AuthController.logIn()
 
 module.exports = AuthController;
--- src/models/authModel.js
+++ src/models/authModel.js
@@ -5,23 +5,34 @@ const prisma = new PrismaClient();
 
 const AuthModel = {
 
-    createUser: async () => {
-        const hashedPassword = await bcrypt.hash("1234", 10);
-
+    createUser: async (password) => {
+        //TODO: tout transformer en variable, un json?
         const result = await prisma.users.create({
             data: {
               username: "lapin",
-              pseudo: "Laura",
+              pseudo: "Lauriane",
               firstname: "Lauriane",
               lastname: "pirouette",
               biography: "ancienne patineuse jeux olympiques",
-              email: "laura.tr@gmail.com",
-              password: hashedPassword,
-              created_at: new Date(),
-              updated_at: new Date(),
+              email: "lauriane.tr@gmail.com",
+              password: password
             },
         });
     },
+
+    logIn: async (email) => {
+        const userInfo = await prisma.users.findUnique({
+            where:{
+                email : email
+            },
+            select:{ 
+                email:true, 
+                password:true
+            }
+        })
+
+        return userInfo
+    }
 }
 
 module.exports = {

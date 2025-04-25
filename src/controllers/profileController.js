c35ca4dd0029f8573d7d2caa88dbe937e391934e
IkuroMaira <gwen.bussac@gmail.com>
2025-04-25 10:36:47 +0200

    Add tracking hashtag in the profile

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -6,10 +6,15 @@ const ProfileController = {
         const userId = req.user.id;
         const connectedUser = await UserModel.getUser(userId);
         const postsByUser = await PostModel.getPostsByUser(userId);
+
+        const processedPersonalPosts = postsByUser.map((post) => {
+            const processedPersonalPost = { ...post};
+            return processedPersonalPost
+        });
         
         res.render("profile", { 
             user: connectedUser,
-            personalPosts: postsByUser
+            personalPosts: processedPersonalPosts,
         });
     }
 }
